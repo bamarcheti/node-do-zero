ef39931a448a21a10a773d36bc61e57a3c2734ed
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-11-07 16:47:34 -0400

    feat: Criação e Integração do Banco de Dados pelo Neon

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import { fastify } from "fastify";
-import { DatabaseMemory } from "./database-memory.js";
+// import { DatabaseMemory } from "./database-memory.js";
+import { DatabasePostgres } from "./database-postgres.js";
 
 const server = fastify();
 
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
 
 // Request Body
 
-server.post("/videos", (request, reply) => {
+server.post("/videos", async (request, reply) => {
   const { title, description, duration } = request.body;
 
-  database.create({
+  await database.create({
     title,
     description,
     duration,
@@ -19,19 +21,19 @@ server.post("/videos", (request, reply) => {
   return reply.status(201).send();
 });
 
-server.get("/videos", (request) => {
+server.get("/videos", async (request) => {
   const search = request.query.search;
 
-  const videos = database.list(search);
+  const videos = await database.list(search);
 
   return videos;
 });
 
-server.put("/videos/:id", (request, reply) => {
+server.put("/videos/:id", async (request, reply) => {
   const videoId = request.params.id;
   const { title, description, duration } = request.body;
 
-  database.update(videoId, {
+  await database.update(videoId, {
     title,
     description,
     duration,
@@ -40,10 +42,10 @@ server.put("/videos/:id", (request, reply) => {
   return reply.status(204).send();
 });
 
-server.delete("/videos/:id", (request, reply) => {
+server.delete("/videos/:id", async (request, reply) => {
   const videoId = request.params.id;
 
-  database.delete(videoId);
+  await database.delete(videoId);
 
   return reply.status(204).send();
 });
